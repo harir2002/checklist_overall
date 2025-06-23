6a8eba30b777686e7c9d24f984148126d52ad11b
harir2002 <hari.r@sbainfo.in>
2025-06-23 10:18:33 +0530

    main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 pages = {
     "Projects": [
+        st.Page("eden.py", title="EDEN")
         st.Page("checklistews.py", title="EWS Checklist"),
         st.Page("Wave City.py", title="Wave City Checklist"),
         st.Page("CheckEligo.py", title="Eligo Checklist"),
