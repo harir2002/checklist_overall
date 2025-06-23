d4852672f900e511a8c979c66421df7728d67e2b
harir2002 <hari.r@sbainfo.in>
2025-06-23 10:56:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 pages = {
     "Projects": [
-        st.Page("veridia.py", title="VERIDIA"),
         st.Page("eden.py", title="EDEN"),
         st.Page("checklistews.py", title="EWS Checklist"),
         st.Page("Wave City.py", title="Wave City Checklist"),
         st.Page("CheckEligo.py", title="Eligo Checklist"),
+        st.Page("veridia.py", title="VERIDIA")
     ]
 }
 
